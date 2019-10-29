43a226af1bc1824bffc86016b0bb756bb8f1fd05
Jigar Sony <sonijigar94@gmail.com>
2019-10-29 23:22:35 +0530

    Added GetRect

---
+++ src/test/java/newFeatures/GetRectMethod.java
@@ -0,0 +1,46 @@
+package newFeatures;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GetRectMethod {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://app.hubspot.com/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		WebElement loginBtn = driver.findElement(By.id("loginBtn"));
+
+		// selenium3
+		Dimension loginBtnDim = loginBtn.getSize();
+		System.out.println(loginBtnDim.getHeight());
+		System.out.println(loginBtnDim.getWidth());
+
+		Point p = loginBtn.getLocation();
+		System.out.println(p.getX());
+		System.out.println(p.getY());
+
+		// selenium4
+		Rectangle loginrect = loginBtn.getRect();
+		System.out.println(loginrect.getHeight());
+		System.out.println(loginrect.getWidth());
+
+		System.out.println(loginrect.getX());
+		System.out.println(loginrect.getY());
+
+		driver.close();
+	}
+
+}
